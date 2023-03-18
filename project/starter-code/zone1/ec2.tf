b149d61ea505b5c4d1903375d91b913bef447aa2
abdo2017 <abdoesam14@gmail.com>
2023-03-18 05:32:17 +0200

    modify ec2 AMIs in zone1 & zone2

--- project/starter-code/zone1/ec2.tf
+++ project/starter-code/zone1/ec2.tf
@@ -3,7 +3,7 @@
    instance_count     = var.instance_count
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-0b7156d8c222b1fb4" 
+   aws_ami            = "ami-0267965cb55f57415" 
    private_subnet_ids = module.vpc.private_subnet_ids
    public_subnet_ids  = module.vpc.public_subnet_ids
    vpc_id             = module.vpc.vpc_id
